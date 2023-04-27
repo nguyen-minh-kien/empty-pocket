f94ebfa0080e18d05f9f2ed686cc537bdf53970e
nguyen-minh-kien <nmkienk44@gmail.com>
2023-04-27 15:34:37 +0700

    set up

---
+++ src/main/java/com/nmkienk44/emptypocket/EmptyPocketApplication.java
@@ -0,0 +1,13 @@
+package com.nmkienk44.emptypocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmptyPocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmptyPocketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nmkienk44/emptypocket/EmptyPocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nmkienk44.emptypocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmptyPocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
